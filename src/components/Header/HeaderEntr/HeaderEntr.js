8c9193693f4fd74c453a9f96e6ab6978658f5d26
MalakhN <nata.malakh89@yandex.ru>
2023-07-06 03:26:16 +0300

    Test

--- src/components/Header/HeaderEntr/HeaderEntr.js
+++ src/components/Header/HeaderEntr/HeaderEntr.js
@@ -4,13 +4,13 @@ import "./HeaderEntr.css";
 function Header() {
   return (
     <div className="header__entrance">
-      <button className="header__entrance-button header__entrance-button_reverse" onClick={handleRegistration}>Регистрация</button>
-      <button className="header__entrance-button" onClick={handleLogIn}>Вход</button>
+      <button className="header__entrance-button header__entrance-button_reverse">Регистрация</button>
+      <button className="header__entrance-button">Вход</button>
       <Link to="/signup" className="header__entrance-button header__entrance-button_reverse">
         Регистрация
       </Link>
       <Link to="/signin" className="header__entrance-button">
-        Войти
+        Вход
       </Link>
     </div>
   );
