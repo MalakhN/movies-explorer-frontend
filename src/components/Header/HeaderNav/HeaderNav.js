0ccf4076fda4daa1b807cd045e75e3d63c146cdc
MalakhN <nata.malakh89@yandex.ru>
2023-07-10 20:19:14 +0300

    Diploma (frontend) +JS: fix after 1 review

--- src/components/Header/HeaderNav/HeaderNav.js
+++ src/components/Header/HeaderNav/HeaderNav.js
@@ -1,12 +1,14 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./HeaderNav.css";
 
 function HeaderNav() {
+  const { pathname } = useLocation();
+
   return (
     <nav className="header__navigation">
       <ul className="header__navigation-list">
-        <li><Link to="/movies"className="header__navigation-link" >Фильмы</Link></li>
-        <li><Link to="/saved-movies" className="header__navigation-link">Сохранённые фильмы</Link></li>
+        <li><Link to="/movies"className={`header__navigation-link" ${pathname === "/movies" ? "header__link_active" : "" }`}>Фильмы</Link></li>
+        <li><Link to="/saved-movies" className={`header__navigation-link" ${pathname === "/saved-movies" ? "header__link_active" : "" }`}>Сохранённые фильмы</Link></li>
       </ul>
     </nav>
   );
