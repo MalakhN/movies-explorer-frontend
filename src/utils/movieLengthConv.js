1a404ff946c3278c8915eb8f9667f63e69f284db
MalakhN <nata.malakh89@yandex.ru>
2023-07-10 19:29:46 +0300

    Diploma (frontend) +JS: fix after 1 review

--- src/utils/movieLengthConv.js
+++ src/utils/movieLengthConv.js
@@ -4,8 +4,12 @@ export function movieLengthConv(mins) {
 
   if (mins < 60) {
     return `${mins} минут`
-  } else if (minutes > 0) {
-    return `${hours}ч ${minutes}м`;
+  } else if (minutes > 0 && hours === 1) {
+    return `${hours} час ${minutes} минут`;
+  } else if (minutes > 0 && hours != 1 && hours <= 4) {
+    return `${hours} часа ${minutes} минут`;
+  } else if (minutes > 0 && hours != 1 && hours > 4) {
+    return `${hours} часов ${minutes} минут`;
   } else if (hours = 1) {
     return `${hours} час`;
   } else if (hours != 1 && hours <= 4) {
