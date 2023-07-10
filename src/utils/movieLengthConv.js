718ca193d1846a58a931daee34c1c2d0bfaeb414
MalakhN <nata.malakh89@yandex.ru>
2023-07-10 19:21:01 +0300

    Diploma (frontend) +JS: fix after 1 review

--- src/utils/movieLengthConv.js
+++ src/utils/movieLengthConv.js
@@ -1,6 +1,6 @@
 export function movieLengthConv(mins) {
-  const hours = Math.floor(mins / 60);
-  const minutes = mins % 60;
+  let hours = Math.floor(mins / 60);
+  let minutes = mins % 60;
 
   if (mins < 60) {
     return `${mins} минут`
