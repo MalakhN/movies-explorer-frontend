b97eb57242c223f3a09a0b379788a92464b57d88
MalakhN <nata.malakh89@yandex.ru>
2023-07-10 20:32:58 +0300

    Diploma (frontend) +JS: fix after 1 review

--- src/components/Header/HeaderNav/HeaderNav.js
+++ src/components/Header/HeaderNav/HeaderNav.js
@@ -7,8 +7,8 @@ function HeaderNav() {
   return (
     <nav className="header__navigation">
       <ul className="header__navigation-list">
-        <li><Link to="/movies"className={`header__navigation-link" ${pathname === "/movies" ? "header__navigation-link_active" : "" }`}>Фильмы</Link></li>
-        <li><Link to="/saved-movies" className={`header__navigation-link" ${pathname === "/saved-movies" ? "header__navigation-link_active" : "" }`}>Сохранённые фильмы</Link></li>
+        <li><Link to="/movies"className={`header__navigation-link ${pathname === "/movies" ? "header__navigation-link_active" : "" }`}>Фильмы</Link></li>
+        <li><Link to="/saved-movies" className={`header__navigation-link ${pathname === "/saved-movies" ? "header__navigation-link_active" : "" }`}>Сохранённые фильмы</Link></li>
       </ul>
     </nav>
   );
